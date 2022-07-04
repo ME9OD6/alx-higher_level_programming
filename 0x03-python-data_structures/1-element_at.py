df2d480ae5d8d8fe1e9157a26027b411fa9d8909
ME9OD6 <melodymburu04@gmail.com>
2022-07-04 10:33:32 +0300

    fix errors

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/pyhton3
 def element_at(my_list, idx):
-    id idx < 0 or idx > len(my_list) - 1:
+    if idx < 0 or idx > len(my_list) - 1:
         return 'None'
     else:
         return(my_list[idx])
